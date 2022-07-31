568af08138f957c21d4680da9a40e7850b989917
Lahfa Samy <samy@lahfa.xyz>
2022-08-01 01:25:43 +0200

    Fix some brew audit warnings

--- metacall.rb
+++ metacall.rb
@@ -1,29 +1,22 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Metacall < Formula
-  desc "MetaCall: The ultimate polyglot programming experience"
   homepage "https://metacall.io/"
+  desc "Ultimate polyglot programming experience"
+  url "https://github.com/metacall/core/archive/refs/tags/v#{version}.tar.gz"
   version "0.5.24"
-  url "https://github.com/metacall/core/archive/refs/tags/v#{version}.zip"
   head "https://github.com/metacall/core", branch: "master"
   license "Apache-2.0"
   # checksum for 0.5.24
   sha256 "04d9f1758dab409e1b1aeb279f78dca2b3b02fb1f59d8574d2457eee04b16f3e"
 
   depends_on "cmake" => :build
+  depends_on "openjdk"
   depends_on "python@3.9"
   depends_on "node@14"
   depends_on "ruby@3.1" 
-  depends_on "java"
 
   
   def install
-    # ENV.deparallelize  # if your formula fails when building in parallel
-    # Remove unrecognized options if warned by configure
-    # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
-    # system "./configure", *std_configure_args, "--disable-silent-rules"
-    system "mkdir build"
+    Dir.mkdir("build")
     Dir.chdir("build")
     args = std_cmake_args + %W[ 
       -Wno-dev
@@ -73,31 +66,11 @@ class Metacall < Formula
     ensure
       file.close unless file.nil?
     end
-    #system "echo" ,"-n", "\#!/usr/bin/env bash >> metacall.sh"
-    #system "echo" ,"-n", "LOC=/usr/local/Cellar/metacall/#{version} >> metacall.sh"
-    ##system "echo" ,"-n", "#CORE_ROOT=$LOC/runtimes/dotnet/shared/Microsoft.NETCore.App/ # TODO: Add DotNet support"
-    #system "echo" ,"-n", "LOADER_LIBRARY=\"$LOC/lib\" >> metacall.sh"
-    #system "echo" ,"-n", "SERIAL_LIBRARY_PATH=\"$LOC/lib\" >> metacall.sh"
-    #system "echo" ,"-n", "DETOUR_LIBRARY_PATH=\"$LOC/lib\" >> metacall.sh"
-    #system "echo" ,"-n", "PORT_LIBRARY_PATH=\"$LOC/lib\" >> metacall.sh"
-    #system "echo" ,"-n", "[[ -n $LOADER_SCRIPT_PATH ]] && LOADER_SCRIPT_PATH=\"$CWD\" >> metacall.sh"
-    #system "echo" ,"-n", "CONFIGURATION_PATH=$LOC/configurations/global.json >> metacall.sh"
-    #system "echo" ,"-n", "$LOC/metacallcli.app/Contents/MacOS/metacallcli $@ >> metacall.sh"
-    system "chmod", "u+x", "metacall.sh"
+    chmod("u+x", "metacall.sh")
     bin.install "metacall.sh" => "metacall"
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test metacall`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    # Creating tests files
     system "echo", "-n", "console.log('Hello from NodeJS') >> testJS.js"
     system "echo", "-n", "print('Hello from Python) >> testPy.py"
     system "echo", "-n", "print('Hello from Ruby) >> testRuby.rb"
