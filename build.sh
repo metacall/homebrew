bb86d05cb6f9aea7c105472ec9d0de9afbe91656
Lahfa Samy <samy@lahfa.xyz>
2022-08-06 17:32:23 +0200

    Add GitHub Actions workflow and simple build.sh

--- build.sh
+++ build.sh
@@ -1,2 +1,6 @@
 #!/usr/bin/env bash
 set -euxo pipefail
+# INSTALL latest brew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+brew install ./metacall.rb --build-from-source -dv
+brew test ./metacall.rb
