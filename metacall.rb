3451ef4ec9c31cc90299723ff67dffe091d6514e
Lahfa Samy <samy@lahfa.xyz>
2022-08-13 15:18:37 +0200

    Update metacall.rb with almost all brew warnings fixed

--- metacall.rb
+++ metacall.rb
@@ -1,19 +1,17 @@
 class Metacall < Formula
   desc "Ultimate polyglot programming experience"
-  homepage "https://metacall.io/"
-  version "0.5.26"
-  url "https://github.com/AkechiShiro/core/archive/refs/heads/metacall-app-bundle-remove.tar.gz"
-  # checksum for 0.5.24
-  sha256 "a029fcaa847e4ea1359227d0cc97577971b1f59a421ec10faa83881e655f2a8d"
+  homepage "https://metacall.io"
+  url "https://github.com/metacall/core/archive/refs/tags/v0.5.27.tar.gz"
+  version "0.5.27"
+  sha256 "1493afadac893b7b3674ea44dd7caa7fd74989880658ae23003b30d3721bb92d"
   license "Apache-2.0"
-  head "https://github.com/metacall/core", branch: "develop"
+  head "https://github.com/metacall/core.git", branch: "develop"
 
   depends_on "cmake" => :build
   depends_on "node@14"
   depends_on "openjdk"
   depends_on "python@3.9"
-  depends_on "ruby@3.1"
-  uses_from_macos "ruby@3.1"
+  uses_from_macos "ruby"
 
   def install
     Dir.mkdir("build")
@@ -57,7 +55,7 @@ class Metacall < Formula
     ]
     cmds = [shebang, *metacall_extra]
     cmds.append("export LOADER_SCRIPT_PATH=\"\${LOADER_SCRIPT_PATH:-\`pwd\`}\"\n")
-    cmds.append("$LOC/metacallcli.app/Contents/MacOS/metacallcli $@\n")
+    cmds.append("$LOC/metacallcli $@\n")
 
     File.open("metacall.sh", "w") do |f|
       f.write(*cmds)
@@ -68,21 +66,41 @@ class Metacall < Formula
   end
 
   test do
-    (testpath/"test.js").write("console.log(\"Hello from NodeJS\")\n")
+    (testpath/"test.js").write <<~EOS
+      console.log("Hello from NodeJS")
+    EOS
     # TypeScript special test
     Dir.mkdir(testpath/"typescript")
-    (testpath/"typescript/typedfunc.ts").write("'use strict';export function typed_sum(left:number,right:number):number{return left+right}export async function typed_sum_async(left:number,right:number):Promise<number>{return left+right}export function build_name(first:string,last='Smith'){return`${first} ${last}`}export function object_pattern_ts({asd}){return asd}export function typed_array(a:number[]):number{return a[0]+a[1]+a[2]}export function object_record(a:Record<string, number>):number{return a.element}")
-
-    (testpath/"test.py").write("print(\"Hello from Python\")\n")
-    (testpath/"test.rb").write("print(\"Hello from Ruby\")\n")
-    (testpath/"test.java").write('public class HelloWorld{public static void main(String[]args)' \
-                                 '{System.err.println("Hello from Java!");System.out.println("Hello from Java!");' \
-                                 'System.out.println("Hello from Java!");System.out.println("Hello from Java!");}}')
+    (testpath/"typescript/typedfunc.ts").write <<~EOS
+      'use strict';
+      export function typed_sum(left:number,right:number):number{return left+right}
+      export async function typed_sum_async(left:number,right:number):Promise<number>{return left+right}
+      export function build_name(first:string,last='Smith'){return`${first} ${last}`}
+      export function object_pattern_ts({asd}){return asd}
+      export function typed_array(a:number[]):number{return a[0]+a[1]+a[2]}
+      export function object_record(a:Record<string, number>):number{return a.element}
+    EOS
+    (testpath/"testTypescript.sh").write <<~EOS
+      #!/usr/bin/env bash
+      cd typescript
+      echo 'load ts typedfunc.ts\ninspect\ncall typed_sum(4, 5)\nexit' | #{bin}/metacall
+    EOS
+    (testpath/"test.py").write <<~EOS
+      print("Hello from Python")
+    EOS
+    (testpath/"test.rb").write <<~EOS
+      print("Hello from Ruby")
+    EOS
+    (testpath/"test.java").write <<~EOS
+      public class HelloWorld{public static void main(String[]args)
+      {System.err.println("Hello from Java!");System.out.println("Hello from Java!");
+      System.out.println("Hello from Java!");System.out.println("Hello from Java!");}}
+    EOS
     # Tests
     assert_match "Hello from Python", shell_output("#{bin}/metacall test.py")
     assert_match "Hello from Ruby", shell_output("#{bin}/metacall test.rb")
     assert_match "Script (test.java) loaded correctly\n", shell_output("#{bin}/metacall test.java")
     assert_match "Hello from NodeJS", shell_output("#{bin}/metacall test.js")
-    assert_match "9.0", shell_output("cd typescript && echo 'load ts typedfunc.ts\ninspect\ncall typed_sum(4, 5)\nexit' | #{bin}/metacall")
+    assert_match "9.0", shell_output(testpath/"testTypescript.sh")
   end
 end
