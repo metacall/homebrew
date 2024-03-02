374f25418134bc1dae23cdf00f8b44a6e3c7ec15
Prakhar Gurunani <prakhargurunani@gmail.com>
2024-03-02 20:48:06 +0530

    wip: fix brew prefix

--- metacall.rb
+++ metacall.rb
@@ -12,6 +12,10 @@ class Metacall < Formula
   depends_on "ruby@2.7"
   depends_on "openjdk"
 
+  def brew_prefix
+    `brew --cellar`
+  end
+
   def python
     deps.map(&:to_formula)
         .find { |f| f.name.match?(/^python@\d\.\d+$/) }
@@ -44,7 +48,7 @@ class Metacall < Formula
       -DOPTION_BUILD_EXAMPLES=OFF
       -DOPTION_BUILD_LOADERS_PY=ON
       -DOPTION_BUILD_LOADERS_NODE=ON
-      -DNodeJS_INSTALL_PREFIX=/usr/local/Cellar/metacall/#{version}
+      -DNodeJS_INSTALL_PREFIX=#{brew_prefix}/metacall/#{version}
       -DOPTION_BUILD_LOADERS_JAVA=ON
       -DOPTION_BUILD_LOADERS_JS=OFF
       -DOPTION_BUILD_LOADERS_C=OFF
@@ -70,7 +74,7 @@ class Metacall < Formula
     # debug = "set -euxo pipefail\n"
 
     metacall_extra = [
-      "PREFIX=/usr/local/Cellar/metacall/#{version}\n",
+      "PREFIX=#{brew_prefix}/metacall/#{version}\n",
       "export LOADER_LIBRARY=\"${PREFIX}/lib\"\n",
       "export SERIAL_LIBRARY_PATH=\"${PREFIX}/lib\"\n",
       "export DETOUR_LIBRARY_PATH=\"${PREFIX}/lib\"\n",
