0a7acee9b79b2314509be11cd6b1f526fa126aab
Prakhar Gurunani <prakhargurunani@gmail.com>
2024-03-04 21:43:50 +0530

    CI: Setup distributable binary (#12)
    * wip: fix commits
    
    * fix: workflow order
    
    * Release do
    
    ---------
    
    Co-authored-by: Vicente Eduardo Ferrer Garcia <7854099+viferga@users.noreply.github.com>

--- build.sh
+++ build.sh
@@ -10,3 +10,9 @@ fi
 # Build metacall brew recipe
 export HOMEBREW_NO_AUTO_UPDATE=1
 brew install ./metacall.rb --build-from-source --overwrite -v
+
+# Build distributable binary using brew pkg
+mkdir pkg && cd pkg
+brew tap timsutton/formulae
+brew install brew-pkg
+brew pkg --with-deps --without-kegs metacall
