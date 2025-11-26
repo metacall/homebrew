478d5ab8847357ebc62d95a25ff185fa0e70d9a4
Vicente Eduardo Ferrer Garcia <vic798@gmail.com>
2025-11-26 23:07:01 +0100

    Add debug and release modes.

--- build.sh
+++ build.sh
@@ -7,6 +7,22 @@ if [[ $(command -v brew) == "" ]]; then
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
+if [ "${1:-}" == "debug" ]; then
+    echo "Build Mode: Debug"
+
+    # Replace the build type by debug
+    sed -i '' '/-DCMAKE_BUILD_TYPE=/c\      -DCMAKE_BUILD_TYPE=Debug' metacall.rb
+
+    # TODO: Add support for preloading address sanitizer in executables using MetaCall
+    # sed -i '/-DCMAKE_BUILD_TYPE=Debug/ a\      -DOPTION_BUILD_ADDRESS_SANITIZER=ON' metacall.rb
+
+elif [ "${1:-}" == "release" ] || [ -z "${1:-}" ]; then
+    echo "Build Mode: Release"
+else
+    echo "Error: Invalid mode. Please use 'debug' or 'release'."
+    exit 1
+fi
+
 # Build metacall brew recipe
 export HOMEBREW_NO_AUTO_UPDATE=1
 brew tap-new metacall/core
